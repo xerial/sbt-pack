5374b12a9fe71ab7edf2a82531a6a007f040693e
Taro L. Saito <leo@xerial.org>
2013-08-20 12:47:32 +0900

    Refactoring multi-module project configuration

--- src/sbt-test/sbt-pack/multi-module/project/Build.scala
+++ src/sbt-test/sbt-pack/multi-module/project/Build.scala
@@ -2,25 +2,29 @@ import sbt._
 import sbt.Keys._
 import xerial.sbt.Pack._
 
+
 object Build extends sbt.Build {
+
+  val commonSettings = Defaults.defaultSettings ++ Seq(
+     scalaVersion := "2.10.1",
+     version := "0.1",
+     crossPaths := false
+  )
 
   lazy val root = Project(
     id = "multi-module",
     base = file("."),
-    settings = Defaults.defaultSettings ++ packSettings ++
+    settings = commonSettings ++ packSettings ++
       Seq(
-        scalaVersion := "2.10.1",
-        packMain := Map("m1" -> "sample.Module1", "m2" -> "sample.Module2"),
+        packMain := Map("m1" -> "sample.Module1", "m2" -> "sample.Module2")
         // custom settings here
-        crossPaths := false
       )
   ) dependsOn(module1, module2)
 
   lazy val module1 = Project(
     id = "module1",
     base = file("module1"),
-    settings = Defaults.defaultSettings ++ Seq(
-      crossPaths := false,
+    settings = commonSettings ++ Seq(
       libraryDependencies += "org.xerial" % "xerial-core" % "3.2.0"
     )
   )
@@ -28,8 +32,7 @@ object Build extends sbt.Build {
   lazy val module2 = Project(
     id = "module2",
     base = file("module2"),
-    settings = Defaults.defaultSettings ++ packSettings ++ Seq(
-      crossPaths := false,
+    settings = commonSettings ++ Seq(
       libraryDependencies += "org.xerial.snappy" % "snappy-java" % "1.1.0-M1"
     )
   )
