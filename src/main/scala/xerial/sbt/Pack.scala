8958774911002fec397b02c8f7cef90f792cd37e
Tom <Tom@Tom.Tom>
2014-01-20 07:44:50 +0100

    removed all jars with specific scope (javadoc, sources) from packed libs

--- src/main/scala/xerial/sbt/Pack.scala
+++ src/main/scala/xerial/sbt/Pack.scala
@@ -86,7 +86,7 @@ object Pack extends sbt.Plugin {
           val mid = m.module
           val me = ModuleEntry(mid.organization, mid.name, mid.revision, artifact.classifier, file.getName)
           me -> file
-        })
+        }).filter(tuple => tuple._1.classifier == None)
 
       val out = streams.value
       val distDir: File = target.value / packDir.value
