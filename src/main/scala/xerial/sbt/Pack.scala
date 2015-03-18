164b332c7ef8f8b2c0c7007a358a5dba2fdfe20c
Egon Nijns <egon.nijns@gmail.com>
2015-03-18 10:09:56 +0100

    added a setting packExcludeArtifactTypes (which defaults to Seq("source", "javadoc", "test") to decide which artifact types to exclude from the package

--- src/main/scala/xerial/sbt/Pack.scala
+++ src/main/scala/xerial/sbt/Pack.scala
@@ -52,6 +52,7 @@ object Pack extends sbt.Plugin with PackArchive {
   val packMain = TaskKey[Map[String, String]]("prog_name -> main class table")
   val packMainDiscovered = TaskKey[Map[String, String]]("discovered prog_name -> main class table")
   val packExclude = SettingKey[Seq[String]]("pack-exclude", "specify projects to exclude when packaging")
+  val packExcludeArtifactTypes = settingKey[Seq[String]]("specify artifact types (e.g. javadoc) to exclude when packaging")
   val packAllClasspaths = TaskKey[Seq[(Classpath, ProjectRef)]]("pack-all-classpaths")
   val packLibJars = TaskKey[Seq[(File, ProjectRef)]]("pack-lib-jars")
   val packGenerateWindowsBatFile = settingKey[Boolean]("Generate BAT file launch scripts for Windows")
@@ -97,6 +98,7 @@ object Pack extends sbt.Plugin with PackArchive {
         allDiscoveredMainClasses.flatMap(_._1.map(mainClass => hyphenize(mainClass.split('.').last) -> mainClass).toMap).toMap
     },
     packExclude := Seq.empty,
+    packExcludeArtifactTypes := Seq("source", "javadoc", "test"),
     packMacIconFile := "icon-mac.png",
     packResourceDir := Map(baseDirectory.value / "src/pack" -> ""),
     packJvmOpts := Map.empty,
@@ -127,7 +129,7 @@ object Pack extends sbt.Plugin with PackArchive {
           (r: sbt.UpdateReport, projectRef) <- packUpdateReports.value
           c <- r.configurations if c.configuration == "runtime"
           m <- c.modules
-          (artifact, file) <- m.artifacts if artifactFilter(`type` = "jar")(c.configuration, m.module, artifact)
+          (artifact, file) <- m.artifacts if !packExcludeArtifactTypes.value.contains(artifact.`type`)
         } yield {
           val mid = m.module
           val me = ModuleEntry(mid.organization, mid.name, VersionString(mid.revision), artifact.name, artifact.classifier, file.getName, projectRef)
