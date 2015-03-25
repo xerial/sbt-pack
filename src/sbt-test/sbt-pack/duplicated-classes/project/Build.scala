e63f7ef3bb48c4f79825b8f3c9e3e265fdc30132
David Perez <david@fccma.com>
2015-03-25 08:23:37 +0100

    Fixed duplicated classes test

--- src/sbt-test/sbt-pack/duplicated-classes/project/Build.scala
+++ src/sbt-test/sbt-pack/duplicated-classes/project/Build.scala
@@ -5,43 +5,37 @@ import xerial.sbt.Pack._
 
 object Build extends sbt.Build {
 
-  val commonSettings = Defaults.defaultSettings ++ packSettings ++ Seq(
+  val commonSettings = packSettings ++ Seq(
     scalaVersion := "2.11.6",
     version := "0.1",
     crossPaths := false
   )
 
-  lazy val root = Project(
-    id = "duplicate-classes",
-    base = file("."),
-    settings = commonSettings ++
+  lazy val root = (project in file(".")).settings(
+    commonSettings ++
       Seq(
         // custom settings here
-      )
+      ): _*
   ) dependsOn(module1, module2)
 
-  lazy val module1 = Project(
-    id = "module1",
-    base = file("module1"),
-    settings = commonSettings ++ Seq(
+  lazy val module1 = project.settings(
+    commonSettings ++ Seq(
       libraryDependencies ++= Seq(
         "org.xerial" % "xerial-core" % "3.3.6",
         "org.slf4j" % "slf4j-api" % "1.7.2" force(),
         "jakarta-regexp" % "jakarta-regexp" % "1.4",
         "xalan" % "xalan" % "2.7.1"
       )
-    )
+    ): _*
   )
 
-  lazy val module2 = Project(
-    id = "module2",
-    base = file("module2"),
-    settings = commonSettings ++ Seq(
+  lazy val module2 = project.settings(
+    commonSettings ++ Seq(
       libraryDependencies ++= Seq(
         "org.xerial.snappy" % "snappy-java" % "1.1.1.6",
         "org.slf4j" % "slf4j-api" % "1.7.6"
       )
-    )
+    ): _*
   )
 
 }
