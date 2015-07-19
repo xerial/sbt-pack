2df33fec82ef57e52eb05d4a2bcf02719ad0abf7
Taro L. Saito <leo@xerial.org>
2015-07-19 16:17:49 -0700

    Allow spaces in finding plubin.sbt paths

--- bin/bump-version.sh
+++ bin/bump-version.sh
@@ -2,8 +2,7 @@
 
 newVersion=`perl -npe "s/version in ThisBuild\s+:=\s+\"(.*)\"/\1/" version.sbt | sed -e "/^$/d"`
 
-for f in $(/bin/ls src/sbt-test/sbt-pack/*/project/plugins.sbt); do \
-echo $f; \
-perl -i -npe "s/addSbtPlugin\(\"org.xerial.sbt\".*/addSbtPlugin\(\"org.xerial.sbt\" % \"sbt-pack\" % \"$newVersion\"\)/" $f; \
+find src/sbt-test/sbt-pack -name "plugins.sbt" -print0 | while read -d $'\0' f; do
+  echo $f;
+  perl -i -npe "s/addSbtPlugin\(\"org.xerial.sbt\".*/addSbtPlugin\(\"org.xerial.sbt\" % \"sbt-pack\" % \"$newVersion\"\)/" "$f";
 done;
-
